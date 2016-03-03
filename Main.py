2fe2b5376dd0d5b226e9d4279ff6032065872d55
Vicky <s52963@beuth-hochschule.de>
2016-03-04 00:17:35 +0100

    2. request (calender) works

--- Main.py
+++ Main.py
@@ -1,25 +1,27 @@
+import re
 import pycurl
 import cStringIO
 import urllib
+import requests
 
-#creates new Stringbuffer, cStringIO faster than StringIO
+###creates new Stringbuffer, cStringIO faster than StringIO
 buff = cStringIO.StringIO()
-#creates new Curl-Object
+###creates new Curl-Object
 c = pycurl.Curl()
 url = "https://tc-app.de/raffauf/tcraffauf.dll/sessionID/$/"
-#set URL
+###set URL
 c.setopt(pycurl.URL, url)
 header = ['user-agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:44.0) Gecko/20100101 Firefox/44.0', 'cache-control: max-age=0']
 c.setopt(pycurl.HTTPHEADER, header)
-#writes response to buffer when performed
+###writes response to buffer when performed
 c.setopt(c.WRITEFUNCTION, buff.write)
-#easier debugging
-c.setopt(c.VERBOSE, True)
-#executes a HTTP-Request
+###easier debugging
+#c.setopt(c.VERBOSE, True)
+###executes a HTTP-Request
 c.perform()
-print buff.getvalue()
+#print buff.getvalue()
 response = buff.getvalue()
-#regex response for new SessionID
+###regex response for new SessionID
 front = "tcraffauf.dll/"
 back = "/$/"
 sessionID = response[(response.index(front)+len(front)):response.index(back)]
@@ -29,17 +31,11 @@ newURL = "https://tc-app.de/raffauf/tcraffauf.dll/" + sessionID + "/$/"
 c.setopt(c.URL, newURL)
 
 #TODO: send new request with sessionID + POSTparameters
-#new Buffer needed?
 newBuff = cStringIO.StringIO()
-data = (('VSTATUS', '0'), ('ARZTWAHL', '0'), ('PATIENTENSTATUS', '0'), ('IW_Action', 'IWGRADBUTTON1'),
+data = (('VSTATUS', '0'), ('ARZTWAHL', '0'), ('PATIENTSTATUS', '0'), ('IW_Action', 'IWGRADBUTTON1'),
         ('IW_ActionParam', ''), ('IW_FormName', 'IWForm1'), ('IW_FormClass', 'TIWForm1'), ('IW_LocationHash', ''),
         ('IW_TrackID', '2'), ('IW_width', '1280'), ('IW_height', '648'), ('IW_SessionID_', sessionID),
         ('IWGRADBUTTON1', ''))
-header.append('Referer: https://tc-app.de/raffauf/tcraffauf.dll/sessionID/$/')
-#set new Header, expects list or tuple of strings
-c.setopt(pycurl.HTTPHEADER, header)
-
-print '----------' + ''.join(header)
 
 c.setopt(c.POST, 1)
 post = urllib.urlencode(data)
@@ -48,8 +44,25 @@ c.setopt(c.WRITEFUNCTION, newBuff.write)
 c.perform()
 print newBuff.getvalue()
 
-#closes buffer
+responseCal = newBuff.getvalue()
+print len(responseCal.split('\n'))
+
+#print type(responseCal)
+
+#print re.search('CS5', responseCal)
+
+#for free in re.finditer('CS5', responseCal):
+#        print free.start(), free.end(), free.group()
+
+#front1 = "SubmitClickConfirm('KALENDER'"
+#back1 = "</a></td><td class=\"KALENDER_CS5\">"
+#cs6 = response[(responseCal.index(front1)+len(front1)):responseCal.index(back1)]
+
+#print cs6
+#print '----------POST ' + ''.join(post)
+
+###closes buffer
 newBuff.close()
 buff.close()
-#ends curl-session
+###ends curl-session
 c.close()
